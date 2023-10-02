da4d65f31ee4dc063251415d2c778e08a7a06b4d
Kush Kaushik <kushkaushik271@gmail.com>
2023-10-02 13:27:54 +0530

    heello

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express();
+
+
+app.get('/' , (req ,res)=>{
+    res.json("hellow world")
+})
+
+
+
+
+
+app.listen(3000 ,() =>{
+    console.log('Connected')
+})
